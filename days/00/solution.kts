f9354bff3798b88ac91d3fb79e4a45c213f7a65d
Mikkel <mikkel@housekeepr.dk>
2023-12-01 23:16:19 +0100

    setup

---
+++ days/00/solution.kts
@@ -0,0 +1,16 @@
+import java.io.File
+
+val testInput = ""
+
+val input = File("input.txt").readText()
+
+private fun first(inp: String): Int {
+    throw NotImplementedError()
+}
+
+private fun second(inp: String): Int {
+    throw NotImplementedError()
+}
+
+println(first(input))
+println(second(input))
